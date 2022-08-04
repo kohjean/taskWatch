cefe4455bd5cd8ffd58ce8275734866e609a18d3
kohjean <58461459+kohjean@users.noreply.github.com>
2022-08-04 18:32:22 +0900

    Add API for update task

--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -5,6 +5,7 @@ module.exports = (app) => {
   router.get('/:id', tasks.findOne);
   router.get('/user/:id', tasks.findAll);
   router.post('/', tasks.create);
+  router.put('/:id', tasks.update);
   router.delete('/:id', tasks.delete);
   app.use('/api/tasks', router);
 };
